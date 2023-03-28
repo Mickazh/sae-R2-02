78ea6fc8f8bebdde6bd2faf5d92c1eb2dd3a2bee
yoyopch <117366105+yoyopch@users.noreply.github.com>
2023-03-28 21:07:13 +0200

    modif class GraphMAdj

--- graphe/GrapheMAdj.java
+++ graphe/GrapheMAdj.java
@@ -1,8 +1,11 @@
 package graphe;
 
 import java.util.List;
+import java.util.Map;
 
 public class GrapheMAdj implements IGraphe{
+    private int[][] matrice;
+    private Map<String,Integer> indices;
 
     @Override
     public List<String> getSommets() {
