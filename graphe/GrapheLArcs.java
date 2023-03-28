4ed220a86d36d44c809b4315b7936a299c070185
Kitsulfr <vidavidphanpro@gmail.com>
2023-03-28 17:39:14 +0200

    test vidavid

---
+++ graphe/GrapheLArcs.java
@@ -0,0 +1,16 @@
+package graphe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrapheLArcs {
+    private List<Arc> arcs;
+
+    public GrapheLArcs() {
+        arcs = new ArrayList<Arc>();
+    }
+
+    public String toString(){
+    return "";
+    }
+}
