3ebce252b923d8df0ff8b9ee1fc9d3d0bda52159
berrre <112023321+berrre@users.noreply.github.com>
2023-03-28 16:52:53 +0200

    ok

--- graphe/GrapheHHAdj.java
+++ graphe/GrapheHHAdj.java
@@ -3,6 +3,5 @@ package graphe;
 public class GrapheHHAdj extends Arc{
     public GrapheHHAdj(String source, String destination, int valeur) {
         super(source, destination, valeur);
-        this.getSource();
     }
 }
