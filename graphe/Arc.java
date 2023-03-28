a1fe6cdc217207bd6bff56cae21ba4816a73e37a
mickael <mickaelzheng40@gmail.com>
2023-03-28 16:01:49 +0200

    ajout classe arc

---
+++ graphe/Arc.java
@@ -0,0 +1,36 @@
+package graphe;
+
+public class Arc {
+    private String source, destination;
+    private int valeur;
+
+    public Arc(String source, String destination, int valeur) {
+        this.source = source;
+        this.destination = destination;
+        this.valeur = valeur;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+}
