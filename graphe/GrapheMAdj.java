30406f15c46daf70bf33da56425932eab25c9547
yoyopch <117366105+yoyopch@users.noreply.github.com>
2023-03-29 12:53:26 +0200

    modif class GraphMAdj

--- graphe/GrapheMAdj.java
+++ graphe/GrapheMAdj.java
@@ -11,13 +11,16 @@ public class GrapheMAdj implements IGraphe{
     //String est la clé
     //Integer est la valeur
     //représente l'indice du sommet
-    public GrapheMAdj(){
+    public GrapheMAdj(int tailleMatrice){
+        matrice=new int[tailleMatrice][tailleMatrice];
         indices=new HashMap<>();
     }
 
     @Override
     public List<String> getSommets() {
-        // TODO Auto-generated method stub
+        List<String > sommets = new ArrayList<>();
+        for(int i = 0; i < indices.size(); ++i){;
+        }
         throw new UnsupportedOperationException("Unimplemented method 'getSommets'");
     }
 
@@ -50,6 +53,9 @@ public class GrapheMAdj implements IGraphe{
     public void ajouterSommet(String noeud) {
         if(!indices.containsKey(noeud)){
             indices.put(noeud, indices.size());
+            for(int i = 0)
+        }else{
+            System.out.println("Sommet deja existant");
         }
 
         throw new UnsupportedOperationException("Unimplemented method 'ajouterSommet'");
@@ -64,7 +70,8 @@ public class GrapheMAdj implements IGraphe{
 
     @Override
     public void oterSommet(String noeud) {
-
+        assert indices.containsKey(noeud);
+        indices.remove(noeud);
         throw new UnsupportedOperationException("Unimplemented method 'oterSommet'");
     }
 
