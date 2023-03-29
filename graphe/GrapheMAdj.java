7306a3f9eeba41e041c013212e1c314fa677a51e
yoyopch <117366105+yoyopch@users.noreply.github.com>
2023-03-29 17:24:56 +0200

    modif class GraphMAdj

--- graphe/GrapheMAdj.java
+++ graphe/GrapheMAdj.java
@@ -1,9 +1,6 @@
 package graphe;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GrapheMAdj implements IGraphe{
     private int[][] matrice;
@@ -18,67 +15,74 @@ public class GrapheMAdj implements IGraphe{
 
     @Override
     public List<String> getSommets() {
+        assert !indices.isEmpty();
         List<String > sommets = new ArrayList<>();
-        for(int i = 0; i < indices.size(); ++i){;
+        for(String s : indices.keySet()){
+            sommets.add(s);
         }
-        throw new UnsupportedOperationException("Unimplemented method 'getSommets'");
+        return sommets;
     }
 
     @Override
     public List<String> getSucc(String sommet) {
+        assert indices.containsKey(sommet);
         List<String> Succ = new ArrayList<>();
-
-        throw new UnsupportedOperationException("Unimplemented method 'getSucc'");
+        int[]valSom = matrice[indices.get(sommet)];
+        for(int i = 0; i < valSom.length; ++i){
+            if (valSom[i]!=0)
+                Succ.add(indices.keySet())
+        }
+        return Succ;
     }
 
     @Override
     public int getValuation(String src, String dest) {
         return matrice[indices.get(src)][indices.get(dest)];
-        throw new UnsupportedOperationException("Unimplemented method 'getValuation'");
     }
 
     @Override
     public boolean contientSommet(String sommet) {
         return indices.containsKey(sommet);
-        throw new UnsupportedOperationException("Unimplemented method 'contientSommet'");
     }
 
     @Override
     public boolean contientArc(String src, String dest) {
         return matrice[indices.get(src)][indices.get(dest)]!=0;
-        throw new UnsupportedOperationException("Unimplemented method 'contientArc'");
     }
 
     @Override
     public void ajouterSommet(String noeud) {
         if(!indices.containsKey(noeud)){
+            int [][]matrice2=new int[indices.size()+1][indices.size()+1];
+            Arrays.fill(matrice2,0);
+            for(int i = 0; i < indices.size(); ++i){
+                for(int j = 0; j< indices.size(); ++j){
+                    matrice2[i][j]=matrice[i][j];
+                }
+            }
+            matrice=matrice2;
             indices.put(noeud, indices.size());
-            for(int i = 0)
         }else{
             System.out.println("Sommet deja existant");
         }
 
-        throw new UnsupportedOperationException("Unimplemented method 'ajouterSommet'");
     }
 
     @Override
     public void ajouterArc(String source, String destination, Integer valeur) {
-            assert indices.containsKey(source) && indices.containsKey(destination);
-            matrice[indices.get(source)][indices.get(destination)]= valeur;
-        throw new UnsupportedOperationException("Unimplemented method 'ajouterArc'");
+        assert indices.containsKey(source) && indices.containsKey(destination);
+        matrice[indices.get(source)][indices.get(destination)]= valeur;
     }
 
     @Override
     public void oterSommet(String noeud) {
         assert indices.containsKey(noeud);
         indices.remove(noeud);
-        throw new UnsupportedOperationException("Unimplemented method 'oterSommet'");
     }
 
     @Override
     public void oterArc(String source, String destination) {
         matrice[indices.get(source)][indices.get(destination)]=0;
-        throw new UnsupportedOperationException("Unimplemented method 'oterArc'");
     }
-    
+
 }
