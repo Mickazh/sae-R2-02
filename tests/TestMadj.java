622895517890192c60c7cab2afe45f89eb3dbee9
YotamWe <yotam.weber@etu.u-paris.fr>
2023-04-03 19:36:05 +0200

    série de tests à valider

--- tests/TestMadj.java
+++ tests/TestMadj.java
@@ -1,10 +1,31 @@
 package tests;
-
 import graphe.GrapheMAdj;
 import main.CheminCourt;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestMadj
 {
+
+    @Test
+    public void Test(){
+        GrapheMAdj graphe = new GrapheMAdj(5);
+        String population = "A-B(8), A-C(6), A-D(2), B-E(4), C-B(3), C-E(4), D-B(5), E:";
+        graphe.peupler(population);
+        assertTrue(graphe.contientSommet("A"));
+        assertTrue(graphe.contientSommet("B"));
+        assertTrue(graphe.contientArc("A","B"));
+        graphe.oterArc("E","B");
+        graphe.ajouterSommet("F");
+        assertTrue(graphe.contientSommet("F"));
+        graphe.ajouterArc("F","C",6);
+        assertTrue(graphe.contientArc("F","C"));
+        assertEquals(8,graphe.getValuation("A","B"));
+        graphe.oterSommet("A");
+        System.out.println(graphe);
+    }
     public static void main(String[] args){
         GrapheMAdj graphe = new GrapheMAdj(5);
         String population = "A-B(8), A-C(6), A-D(2), B-E(4), C-B(3), C-E(4), D-B(5), E:";
